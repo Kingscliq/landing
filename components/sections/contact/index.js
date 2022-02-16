a956265c51371fd953cf36bd8e6266d0378cc90e
ajaezokingsley <ajaezokingsley@gmail.com>
2022-02-16 02:25:02 -0800

    adjustment

--- components/sections/contact/index.js
+++ components/sections/contact/index.js
@@ -14,10 +14,10 @@ const Contact = () => {
   return (
     <section className="bg-white px-0 md:px-16">
       <div className="grid grid-cols-1 h-auto md:grid-cols-3 md:h-[40.125rem] container md:gap-10 items-center ">
-        {/* <div className="col-span-1 bg-contact bg-cover h-[40.125rem] mb-16 md:mb-auto px-10 flex items-start flex-col justify-between"> */}
-        <div
-          className={`col-span-1 bg-["url('${contactArea.src}')"] bg-cover h-[40.125rem] mb-16 md:mb-auto px-10 flex items-start flex-col justify-between"`}
-        >
+        <div className="col-span-1 bg-contact bg-cover h-[40.125rem] mb-16 md:mb-auto px-10 flex items-start flex-col justify-between">
+          {/* <div
+          className={`col-span-1 bg-["url('${contactArea.src})"] bg-cover h-[40.125rem] mb-16 md:mb-auto px-10 flex items-start flex-col justify-between"`}
+        > */}
           <div>
             <Subheading className="text-white mt-20 mb-5">
               Contact us today
